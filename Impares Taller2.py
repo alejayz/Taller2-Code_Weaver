41f0af36706dd705319fc5f6e0ecb3f50fda4aac
Alejandra Arenas <124609988+alejayz@users.noreply.github.com>
2023-10-28 21:33:20 -0500

    Update Impares Taller2.py

--- Impares Taller2.py
+++ Impares Taller2.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     
 #3. Desarrollar un programa que permita ingresar dos números enteros y determinar si se tratan de números espejos.
 
-def invertir_numero(a : int) -> int: #la funcion recibe un entero y retorna un entero
+def invertirNumero(a : int) -> int: #la funcion recibe un entero y retorna un entero
     invertido = 0 #variable para almacenar el numero invertido
     while a != 0: #condición de número diferente de cero para iniciar el ciclo
         invertido = 10*invertido+a % 10 #toma los digitos de atras hacia adelante
@@ -28,7 +28,7 @@ def invertir_numero(a : int) -> int: #la funcion recibe un entero y retorna un e
 if __name__== "__main__":
     a= int(input("ingrese un numero entero: "))
     b = int(input("ingrese un numero entero: "))
-    invert = invertir_numero(a)
+    invert = invertirNumero(a)
     if b == invert:
         print(str(a) + " y " + str(b) + " son números espejos")
     else:
